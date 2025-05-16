50880acc237ab2ac56990a3d553d2c2418dfe44b
Oleg May <alpha_may@naver.com>
2025-05-17 05:51:44 +0700

    port 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 2052
+const port = 80
 const cors = require('cors');
 const WebSocket = require('ws');
 const fs = require('fs');
